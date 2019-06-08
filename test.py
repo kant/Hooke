6dd072d22f6753602670be179924b3a0e5d0c8a1
oekshido <joseugarte2262005@gmail.com>
2019-06-08 09:42:35 -0700

    Fixed Normalizing and Preprocessing

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-import search, extract
-
+import search, extract, pickle
 read = search.read("test.txt")
-search = search.search(read)
-ex = extract.text(search)
-for x in ex:
-    print( extract.preprocess(extract.normalize(x) ))
-print(len(extract),"texts")
+# search = search.search(read)
+# ex = extract.text(search)
+with open("ex","rb") as f:
+    ex = pickle.load(f)
+print(extract.preprocess(extract.normalize(ex)))
+print(len(ex),"texts")
+print("Ready")
