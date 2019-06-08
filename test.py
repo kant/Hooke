677aa8ae3b28a8e9152568a3cfcbec5b9a707517
oekshido <joseugarte2262005@gmail.com>
2019-06-08 11:04:24 -0700

    Finished and tested compare

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-import search, extract, pickle
+import search, extract, pickle, compare
 read = search.read("test.txt")
+read = extract.preprocess(extract.normalize(read))[0]
 # search = search.search(read)
 # ex = extract.text(search)
 with open("ex","rb") as f:
     ex = pickle.load(f)
-print(extract.preprocess(extract.normalize(ex)))
-print(len(ex),"texts")
-print("Ready")
+texts = extract.preprocess(extract.normalize(ex))
+ohNo= compare.compare(read, texts, threshold=13,length=15)
+for upsie in ohNo:
+    match, index, text = upsie
+    (start, end, diff) = match
+    x = texts[index]
+    y = x[start:end]
+    print(y, "IS CLOSE TO",text,"BY",diff)
