4c75bc6ba69baf9538bd204658468d1e99b32159
oekshido <joseugarte2262005@gmail.com>
2019-06-11 18:51:35 -0700

    Fixing

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ norl = 7
 pret = 0
 prel = 10
 timeout = 10
+lang = "english"
 ##/vars
 
-norread = search.read(inp)
-preread = extract.preprocess(extract.normalize(norread))[0]
-search = search.search(norread)
-ex = extract.text(search, timeout=timeout)
-nortexts = extract.normalize(ex)
-pretext = extract.preprocess(nortexts)
-norcom = compare.compare(norread, nortexts, threshold=nort,length=norl)
-precom = compare.compare(preread, pretext, threshold=pret, length=prel)
+extract = extract.ExtractC()
 
-print(norcom, precom)
+read = search.read(inp)
+searches = search.div(read)
+print(read)
+norread = extract.normalize(read)
+preread = extract.preprocess(norread)
+print(norread, preread, searches)
+# search = search.search(read)
+# ex = extract.text(search, timeout=timeout)
+# nortexts = extract.normalize(ex)
+# pretext = extract.preprocess(nortexts)
+# norcom = compare.compare(norread, nortexts, threshold=nort,length=norl)
+# precom = compare.compare(preread, pretext, threshold=pret, length=prel)
+
+# print(norcom, precom)
