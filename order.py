f58eb061f6b4b904b80c0834953223c3676805c9
oekshido <joseugarte2262005@gmail.com>
2019-06-21 16:51:40 -0700

    Cleaned up code

---
+++ order.py
@@ -0,0 +1,29 @@
+def match_elements(matches):
+    #Extract match elements
+    output = []
+    for y in matches:
+        match, source, text = y
+        output.append([match.dist, text, source, match.start, match.end])
+    return output
+
+def source_sort(matches, leng):
+    #Orginize by source
+    output = [[]for k in range(leng)]
+    for x in matches:
+        output[x[2]].append(x)
+    return output
+
+def check_merges(matches):
+    output = []
+    for z in matches:
+        try:
+                results = [(z[0],0)]
+                count = 0
+                for match in z[1:]:
+                        if match[3] > results[-1][0][4]:
+                                count += 1
+                        results.append((match, count))
+                output.append(results)
+        except IndexError:
+                output.append(None)
+    return output
