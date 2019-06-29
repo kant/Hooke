401327b63e969e3cafc862ac1e9efcbcac5b7360
oekshido <joseugarte2262005@gmail.com>
2019-06-28 19:03:49 -0700

    Tested

---
+++ Hooke/order.py
@@ -0,0 +1,74 @@
+def match_elements(matches):
+    #Extract match elements
+    output = []
+    for y in matches:
+        match, source, text = y
+        output.append([match.dist, text, source, match.start, match.end])
+    return output
+
+def source_sort(matches, leng):
+    #Orginize by source
+    output = [[]for k in range(leng)]
+    for x in matches:
+        output[x[2]].append(x)
+    return output
+
+def check_merges(matches):
+    #Checks merging matches
+    output = []
+    for source in matches:
+        try:
+                results = [(source[0],0)]
+                count = 0
+                for match in source[1:]:
+                        if match[3] > results[-1][0][4]:
+                                count += 1
+                        results.append((match, count))
+                output.append(results)
+        except IndexError:
+                output.append(None)
+    return output
+
+def print_matches(matches, searchurls, ex = []):
+    #Prints Matches
+    count = 0
+    exclude = []
+    for source in matches:
+            try:
+                    if count not in ex:
+                        print(len(source),"match (es) from", searchurls[count])
+                        exclude.append(count)
+            except:
+                    pass
+            count += 1
+    return exclude
+
+def separate_matches(matches):
+        output = []
+        for source in matches:
+                try:
+                        ts = [[] for x in range(source[-1][-1] + 1)]
+                        for match in source:
+                                ts[match[1]].append(match[0])
+                        output.append(ts)
+                except:
+                        output.append(None)
+        return output
+
+
+def join_matches(matches):
+        output = []
+        for source in matches:
+                ns = []
+                try:
+                        for match in source:
+                                tex = match[0][1][:-1]
+                                added = 0
+                                for m in match:
+                                        added += m[0]
+                                        tex.append(m[1][-1])
+                                ns.append([added/len(match), tex, match[0][3],match[-1][4]])
+                except:
+                        ns = None
+                output.append(ns)
+        return output
---
+++ Hooke/search.py
@@ -0,0 +1,34 @@
+import textract
+from google import google
+
+def read(file, every=16, length=32):
+    # Reads from file
+    raw = textract.process(file)
+    words = raw.decode().split()
+    return words
+
+def div(words, every=16, length=32):
+    # Divides the text in google-friendly 32 word text in 16 word intervals
+    search = []
+    count = 0
+    run = True
+    while run:
+        current = ""
+        for x in range(count * every, count * every + length):
+            try:
+                current = current + " " + words[x]
+            except:
+                run = False
+        search.append(current)
+        count += 1
+    return search
+
+def search(searches, results=1):
+    # Searches divided text in google
+    list = []
+    for x in searches:
+        for element in google.search(x, results):
+            url = element.link
+            if url not in list:
+                list.append(url)
+    return list
